b5d0209edef1a2962d44a81e4430afb6e4ee98d4
maarcf <marr.cairo@gmail.com>
2025-05-07 19:34:54 -0300

    chore(): start project

---
+++ ex-2.py
@@ -0,0 +1,10 @@
+# Una Administradora de Consorcios necesita un sistema para poder gestionar el
+# cobro de las expensas de un edificio de departamentos de 20 unidades. En dos
+# listas almacena la siguiente información: Número de unidad y superficie en metros
+# cuadrados. Validar que no se ingresen números de unidades duplicadas. Cada
+# unidad paga de expensas un valor fijo por metro cuadrado, el que se ingresa
+# por teclado. Se pide:
+# · Informar el promedio de expensas del mes.
+# · Ordenar los listados de mayor a menor según la superficie. Mostrar por
+# pantalla el listado ordenado informando el número de unidad y la superficie
+# en metros cuadrados.
